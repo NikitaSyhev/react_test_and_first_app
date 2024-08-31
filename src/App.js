54b5c907e8a44e0e4fcd8b659502d3c80a11a384
Nikita Sychev <nikitasychev@inbox.ru>
2024-08-31 18:18:32 +0500

    sec com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+
+
+
+const Header = () => {
+  return <h2>Hello, world!</h2>
+}
+
+
+// const Field = () => {
+//   const holder = 'Enter here';
+//   const styledFiled = {
+//     width: '300px',
+//   }
+//   return <input 
+//             placeholder={holder} 
+//             type='text' 
+//             style={styledFiled}/>
+// }
+
+
+//создание компонента через класс
+class Field extends React.Component {
+      render() {
+        const holder = 'Enter here';
+        const styledFiled = {
+          width: '300px',
+      };
+      return <input 
+      placeholder={holder} 
+      type='text' 
+      style={styledFiled}/>
+}
+}
+
+function Btn() {
+  const text = 'Log in';
+
+  //examples
+  // const res = () => {
+  //   return 'Log in';
+  // }
+  // const p = <p>Log in</p>;
+
+  const logged = true;
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <Field/>
+        <Btn/>
     </div>
   );
 }
 
 export default App;
+export {Header};
--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Header } from './App';
 
 
 const text = 'Hello, world!';
 
 const elem = (
   <div>
-      <h2 className='text'>{text}</h2>
-      <input type="text" />
-      <label>Button</label>
-      <button tabIndex='0' className='button'>Click</button>
+      <App/>
   </div>
 );
 
