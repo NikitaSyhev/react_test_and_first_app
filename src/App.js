f11b4f5b04a24e0d91b975d136ae3564ccaf1830
Nikita Sychev <nikitasychev@inbox.ru>
2025-01-04 12:00:04 +0500

    add REACT STYLED COMPONENT

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
 
 import './App.css';
-import React, {Component} from 'react';
+
+
+//переменная для styled component
+//поддерживает вложенность, т.е. если внутри EmpItem есть ссылка ( а )
+// то применяются стили a{.....}
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+  a{
+    display: block;
+    margin: 10px 0 10px 0;
+    color: ${props => props.active ? 'orange' : 'black'};
+  }
+  imput {
+    display: block;
+    margin-top: 10px; 
+  }
+`;
+
+const Header = styled.h2`
+  font-size: 22px;
+
+`;
+
+
+//экспортируем стили для использования стилей компонентов в другом файле
+export const Button = styled.button`
+  display: block;
+  padding: 5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0, 0, 0, .2);
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+`;
 
 
 
@@ -17,45 +53,58 @@ class WhoAmI extends Component {
     // this.nextYear = this.nextYear.bind(this);
   }
 
-  nextYear() {
+  nextYear = () => {
     this.setState(state => ({
       years: state.years + 1,
     }))
   }
 
-  commitInputChanges = (e) => {
+  //здесь мы принимаем 2 аргумента: e + some color
+  commitInputChanges = (e, color) => {
+    //пример использования 2го аргумента
+    console.log(color);
     this.setState({
       position: e.target.value,
     })
   }
 
-
-  return() {
+  render() {
     const {years, position} = this.state;
     const {name, surname, link} = this.props;
-      <div>
-        <button onClick={this.nextYear}>+++</button>
-        <h1>My name is {name}, surname - {surname}, 
+    return(
+      <EmpItem active>
+        <Button onClick={this.nextYear}>+++</Button>
+        <Header>My name is {name}, surname - {surname}, 
           age {years},
-          position {position
-          }</h1>
+          position {position},
+          ссылка на профиль - {link}
+          </Header>
           <form >
             <span>Введите должность</span>
-            <input type="text" onChange={this.commitInputChanges} />
+            <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
           </form>
         <a href="{props.link}">My profile</a>
-      </div>
+      </EmpItem>
+      )
+    }
   }
-}
 
+
+  //создали react styled component
+  const Wrapper = styled.div`
+    width: 600px;
+    margin: 80px auto 0 auto;
+  `;
 
 
 function App() {
   return (
-    <div className="App">
-        <WhoAmI name={()=>{return 'Nikita'}} surname="Sychev" link="nikitasychev.ru"/>  
-        <WhoAmI name={()=>{return 'Nikita'}} surname="SychevNikita" link="nikitasychev.ru"/>
-    </div>
+    
+      <Wrapper>
+          <WhoAmI name={()=>{return 'Nikita'}} surname="Sychev" link="nikitasychev.ru"/>  
+          <WhoAmI name={()=>{return 'Nikita'}} surname="SychevNikita" link="nikitasychev.ru"/>
+      </Wrapper>
+      
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+//импорт стилей компонентов 
+import { Button } from './App';
+import styled from 'styled-components';
 
+//наследование стилей (для изменения стилей в импортируемой Button)
+
+const BigButton = styled(Button)`
+  margin: 0 auto;
+  width: 245px;
+  text-align: center;
+`;
 
 
 const text = 'Hello, world!';
@@ -10,8 +20,11 @@ const text = 'Hello, world!';
 const elem = (
   <StrictMode>
       <App/>
+      <BigButton as="a">Отправить отчет</BigButton> 
   </StrictMode>  
 );
+
+//as a - меняем тег с Button на ссылку ( a )
 
 // const elem = React.createElement('h2', {className: 'greeting'}, 'Hello, world!');
 
